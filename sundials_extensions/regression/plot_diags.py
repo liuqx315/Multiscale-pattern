49b9fc77b5179c65cd9c0ac8c56373a3a1034fa1
Daniel R. Reynolds <reynolds@smu.edu>
2012-09-03 16:28:10 -0500

    renamed arkode_tools.py to diag_tools.py to properly reflect its actions.

--- sundials_extensions/regression/plot_diags.py
+++ sundials_extensions/regression/plot_diags.py
@@ -3,23 +3,23 @@
 
 # imports
 import sys
-import arkode_tools as ark
+import diag_tools as diags
 
 # load the diagnostics output file (passed on the command line)
 fname = sys.argv[len(sys.argv)-1];
 
 # load the time step data 
-Tdiags = ark.load_diags(fname);
+Tdiags = diags.load_diags(fname);
 
 # generate plots 
-ark.plot_h_vs_t(Tdiags,'h_vs_t.png');
-ark.plot_h_vs_iter(Tdiags,'h_vs_iter.png');
-ark.plot_work_vs_t(Tdiags,'work_vs_t.png');
-ark.plot_work_vs_h(Tdiags,'work_vs_h.png');
-ark.plot_oversolve_vs_t(Tdiags,'oversolve_vs_t.png');
+diags.plot_h_vs_t(Tdiags,'h_vs_t.png');
+diags.plot_h_vs_iter(Tdiags,'h_vs_iter.png');
+diags.plot_work_vs_t(Tdiags,'work_vs_t.png');
+diags.plot_work_vs_h(Tdiags,'work_vs_h.png');
+diags.plot_oversolve_vs_t(Tdiags,'oversolve_vs_t.png');
 
 # print solve statistics to screen
-ark.etest_stats(Tdiags, sys.stdout);
-ark.solver_stats(Tdiags, sys.stdout);
+diags.etest_stats(Tdiags, sys.stdout);
+diags.solver_stats(Tdiags, sys.stdout);
 
 ##### end of script #####
