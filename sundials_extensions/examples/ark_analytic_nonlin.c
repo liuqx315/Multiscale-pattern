aeec751b6319b301d2b67af95f950b09f9479c75
Daniel R. Reynolds <reynolds@smu.edu>
2013-06-06 22:24:08 -0500

    Updated ark_analytic_nonlin example to use ERK method.

--- sundials_extensions/examples/ark_analytic_nonlin.c
+++ sundials_extensions/examples/ark_analytic_nonlin.c
@@ -13,9 +13,7 @@
  * This has analytical solution 
  *      y(t) = log(0.5*t^2 + t + 1)
  * 
- * This program solves the problem with the DIRK method,
- * Newton iteration with the ARKDENSE dense linear solver, and a
- * user-supplied Jacobian routine.
+ * This program solves the problem with the ERK method.
  * Output is printed every 1.0 units of time (10 total).
  * Run statistics (optional outputs) are printed at the end.
  * -----------------------------------------------------------------*/
@@ -25,15 +23,10 @@
 #include <math.h>
 #include <arkode/arkode.h>
 #include <nvector/nvector_serial.h>
-#include <arkode/arkode_dense.h>
-#include <sundials/sundials_dense.h>
 #include <sundials/sundials_types.h>
 
 /* User-supplied Functions Called by the Solver */
 static int f(realtype t, N_Vector y, N_Vector ydot, void *user_data);
-static int Jac(long int N, realtype t,
-               N_Vector y, N_Vector fy, DlsMat J, void *user_data,
-               N_Vector tmp1, N_Vector tmp2, N_Vector tmp3);
 
 /* Private function to check function return values */
 static int check_flag(void *flagvalue, char *funcname, int opt);
@@ -54,7 +47,6 @@ int main()
   void *arkode_mem = NULL;
 
   /* read problem parameter and tolerances from input file:
-     lamda  - problem stiffness parameter
      reltol - desired relative tolerance
      abstol - desired absolute tolerance */
   double reltol_, abstol_;
@@ -92,7 +84,7 @@ int main()
   /* Call ARKodeInit to initialize the integrator memory and specify the
      user's right hand side function in y'=f(t,y), the inital time T0, and
      the initial dependent variable vector y */
-  flag = ARKodeInit(arkode_mem, NULL, f, T0, y);
+  flag = ARKodeInit(arkode_mem, f, NULL, T0, y);
   if (check_flag(&flag, "ARKodeInit", 1)) return 1;
 
   /* Call ARKodeSetDiagnostics to set diagnostics output file pointer */
@@ -103,14 +95,6 @@ int main()
      tolerances */
   flag = ARKodeSStolerances(arkode_mem, reltol, abstol);
   if (check_flag(&flag, "ARKodeSStolerances", 1)) return 1;
-
-  /* Call ARKDense to specify the ARKDENSE dense linear solver */
-  flag = ARKDense(arkode_mem, NEQ);
-  if (check_flag(&flag, "ARKDense", 1)) return 1;
-
-  /* Set the Jacobian routine to Jac (user-supplied) */
-  flag = ARKDlsSetDenseJacFn(arkode_mem, Jac);
-  if (check_flag(&flag, "ARKDlsSetDenseJacFn", 1)) return 1;
 
   /* In loop, call ARKode, print results, and test for error.
      Break out of loop when the final output time has been reached */
@@ -134,34 +118,19 @@ int main()
   printf("   ---------------------\n");
 
   /* Print some final statistics */
-  long int nst, nst_a, nfe, nfi, nsetups, nje, nfeLS, nni, ncfn, netf;
+  long int nst, nst_a, nfe, nfi, netf;
   flag = ARKodeGetNumSteps(arkode_mem, &nst);
   check_flag(&flag, "ARKodeGetNumSteps", 1);
   flag = ARKodeGetNumStepAttempts(arkode_mem, &nst_a);
   check_flag(&flag, "ARKodeGetNumStepAttempts", 1);
   flag = ARKodeGetNumRhsEvals(arkode_mem, &nfe, &nfi);
   check_flag(&flag, "ARKodeGetNumRhsEvals", 1);
-  flag = ARKodeGetNumLinSolvSetups(arkode_mem, &nsetups);
-  check_flag(&flag, "ARKodeGetNumLinSolvSetups", 1);
   flag = ARKodeGetNumErrTestFails(arkode_mem, &netf);
   check_flag(&flag, "ARKodeGetNumErrTestFails", 1);
-  flag = ARKodeGetNumNonlinSolvIters(arkode_mem, &nni);
-  check_flag(&flag, "ARKodeGetNumNonlinSolvIters", 1);
-  flag = ARKodeGetNumNonlinSolvConvFails(arkode_mem, &ncfn);
-  check_flag(&flag, "ARKodeGetNumNonlinSolvConvFails", 1);
-  flag = ARKDlsGetNumJacEvals(arkode_mem, &nje);
-  check_flag(&flag, "ARKDlsGetNumJacEvals", 1);
-  flag = ARKDlsGetNumRhsEvals(arkode_mem, &nfeLS);
-  check_flag(&flag, "ARKDlsGetNumRhsEvals", 1);
 
   printf("\nFinal Solver Statistics:\n");
   printf("   Internal solver steps = %li (attempted = %li)\n", nst, nst_a);
   printf("   Total RHS evals:  Fe = %li,  Fi = %li\n", nfe, nfi);
-  printf("   Total linear solver setups = %li\n", nsetups);
-  printf("   Total RHS evals for setting up the linear system = %li\n", nfeLS);
-  printf("   Total number of Jacobian evaluations = %li\n", nje);
-  printf("   Total number of Newton iterations = %li\n", nni);
-  printf("   Total number of linear solver convergence failures = %li\n", ncfn);
   printf("   Total number of error test failures = %li\n\n", netf);
 
   /* Free y vector */
@@ -185,15 +154,6 @@ int main()
 static int f(realtype t, N_Vector y, N_Vector ydot, void *user_data)
 {
   NV_Ith_S(ydot,0) = (t+1.0)*exp(-NV_Ith_S(y,0));
-  return 0;
-}
-
-/* Jacobian routine to compute J(t,y) = df/dy. */
-static int Jac(long int N, realtype t,
-               N_Vector y, N_Vector fy, DlsMat J, void *user_data,
-               N_Vector tmp1, N_Vector tmp2, N_Vector tmp3)
-{
-  DENSE_ELEM(J,0,0) = -(t+1.0)*exp(-NV_Ith_S(y,0));
   return 0;
 }
 
