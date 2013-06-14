c9ee0b9110f96f38ccefe95b474296917af729ba
Daniel R. Reynolds <reynolds@smu.edu>
2013-06-13 20:51:45 -0500

    Added Hilari's FGMRES linear solver to ARKode

--- sundials_extensions/arkode/arkode_spfgmr.c
+++ sundials_extensions/arkode/arkode_spfgmr.c
@@ -4,100 +4,99 @@
 ----------------------------------------------------------------- 
  Programmer(s): Daniel R. Reynolds @ SMU
 -----------------------------------------------------------------
- This is the implementation file for the ARKPCG linear solver.
+ This is the implementation file for the ARKSPFGMR linear solver.
 ---------------------------------------------------------------*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <arkode/arkode_pcg.h>
+#include <arkode/arkode_spfgmr.h>
 #include "arkode_spils_impl.h"
 #include "arkode_impl.h"
 
-/* #include <sundials/sundials_pcg.h> */
-#include <arkode/pcg/sundials_pcg.h>
+/* #include <sundials/sundials_spfgmr.h> */
+#include <arkode/spfgmr/sundials_spfgmr.h>
 #include <sundials/sundials_math.h>
 
 /* Constants */
 #define ZERO RCONST(0.0)
 #define ONE  RCONST(1.0)
 
-/* ARKPCG linit, lsetup, lsolve, and lfree routines */
-static int ARKPcgInit(ARKodeMem ark_mem);
-static int ARKPcgSetup(ARKodeMem ark_mem, int convfail, 
-		       N_Vector ypred, N_Vector fpred, 
-		       booleantype *jcurPtr, N_Vector vtemp1,
-		       N_Vector vtemp2, N_Vector vtemp3);
-static int ARKPcgSolve(ARKodeMem ark_mem, N_Vector b, 
-		       N_Vector weight, N_Vector ynow, 
-		       N_Vector fnow);
-static void ARKPcgFree(ARKodeMem ark_mem);
+/* ARKSPFGMR linit, lsetup, lsolve, and lfree routines */
+static int ARKSpfgmrInit(ARKodeMem ark_mem);
+static int ARKSpfgmrSetup(ARKodeMem ark_mem, int convfail, 
+			  N_Vector ypred, N_Vector fpred, 
+			  booleantype *jcurPtr, N_Vector vtemp1,
+			  N_Vector vtemp2, N_Vector vtemp3);
+static int ARKSpfgmrSolve(ARKodeMem ark_mem, N_Vector b, 
+			  N_Vector weight, N_Vector ynow, 
+			  N_Vector fnow);
+static void ARKSpfgmrFree(ARKodeMem ark_mem);
 
 
 /*---------------------------------------------------------------
- ARKPcg:
+ ARKSpfgmr:
 
  This routine initializes the memory record and sets various 
- function fields specific to the Pcg linear solver module. 
- ARKPcg first calls the existing lfree routine if this is not 
- NULL. It then sets the ark_linit, ark_lsetup, ark_lsolve, 
- ark_lfree fields in (*arkode_mem) to be ARKPcgInit, 
- ARKPcgSetup, ARKPcgSolve, and ARKPcgFree, respectively. 
- It allocates memory for a structure of type ARKSpilsMemRec and 
- sets the ark_lmem field in (*arkode_mem) to the address of 
- this structure. It sets setupNonNull in (*arkode_mem),
- and sets various fields in the ARKSpilsMemRec structure.
- Finally, ARKPcg allocates memory for ytemp and x, and calls
- PcgMalloc to allocate memory for the Pcg solver.
+ function fields specific to the Spfgmr linear solver module. 
+ ARKSpfgmr first calls the existing lfree routine if this is not 
+ NULL.  It then sets the ark_linit, ark_lsetup, ark_lsolve, 
+ ark_lfree fields in (*arkode_mem) to be ARKSpfgmrInit, 
+ ARKSpfgmrSetup, ARKSpfgmrSolve, and ARKSpfgmrFree, respectively.  
+ It allocates memory for a structure of type ARKSpilsMemRec and
+ sets the ark_lmem field in (*arkode_mem) to the address of this
+ structure.  It sets setupNonNull in (*arkode_mem), and sets 
+ various fields in the ARKSpilsMemRec structure. Finally, 
+ ARKSpfgmr allocates memory for ytemp and x, and calls
+ SpfgmrMalloc to allocate memory for the Spfgmr solver.
 ---------------------------------------------------------------*/
-int ARKPcg(void *arkode_mem, int pretype, int maxl)
+int ARKSpfgmr(void *arkode_mem, int pretype, int maxl)
 {
   ARKodeMem ark_mem;
   ARKSpilsMem arkspils_mem;
-  PcgMem pcg_mem;
+  SpfgmrMem spfgmr_mem;
   int mxl;
 
   /* Return immediately if arkode_mem is NULL */
   if (arkode_mem == NULL) {
-    ARKProcessError(NULL, ARKSPILS_MEM_NULL, "ARKPCG", 
-		    "ARKPcg", MSGS_ARKMEM_NULL);
+    ARKProcessError(NULL, ARKSPILS_MEM_NULL, "ARKSPFGMR", 
+		    "ARKSpfgmr", MSGS_ARKMEM_NULL);
     return(ARKSPILS_MEM_NULL);
   }
   ark_mem = (ARKodeMem) arkode_mem;
 
-  /* Check if N_VDotProd and N_VWrmsNorm is present */
-  if ((ark_mem->ark_tempv->ops->nvdotprod == NULL) ||
-      (ark_mem->ark_tempv->ops->nvwrmsnorm == NULL)) {
-    ARKProcessError(ark_mem, ARKSPILS_ILL_INPUT, "ARKPCG", 
-		    "ARKPcg", MSGS_BAD_NVECTOR);
+  /* Check if N_VDotProd is present */
+  if(ark_mem->ark_tempv->ops->nvdotprod == NULL) {
+    ARKProcessError(ark_mem, ARKSPILS_ILL_INPUT, "ARKSPFGMR", 
+		    "ARKSpfgmr", MSGS_BAD_NVECTOR);
     return(ARKSPILS_ILL_INPUT);
   }
 
   if (ark_mem->ark_lfree != NULL) ark_mem->ark_lfree(ark_mem);
 
   /* Set four main function fields in ark_mem */
-  ark_mem->ark_linit  = ARKPcgInit;
-  ark_mem->ark_lsetup = ARKPcgSetup;
-  ark_mem->ark_lsolve = ARKPcgSolve;
-  ark_mem->ark_lfree  = ARKPcgFree;
+  ark_mem->ark_linit  = ARKSpfgmrInit;
+  ark_mem->ark_lsetup = ARKSpfgmrSetup;
+  ark_mem->ark_lsolve = ARKSpfgmrSolve;
+  ark_mem->ark_lfree  = ARKSpfgmrFree;
 
   /* Get memory for ARKSpilsMemRec */
   arkspils_mem = NULL;
   arkspils_mem = (ARKSpilsMem) malloc(sizeof(struct ARKSpilsMemRec));
   if (arkspils_mem == NULL) {
-    ARKProcessError(ark_mem, ARKSPILS_MEM_FAIL, "ARKPCG", 
-		    "ARKPcg", MSGS_MEM_FAIL);
+    ARKProcessError(ark_mem, ARKSPILS_MEM_FAIL, "ARKSPFGMR", 
+		    "ARKSpfgmr", MSGS_MEM_FAIL);
     return(ARKSPILS_MEM_FAIL);
   }
 
   /* Set ILS type */
-  arkspils_mem->s_type = SPILS_PCG;
+  arkspils_mem->s_type = SPILS_SPFGMR;
 
-  /* Set Pcg parameters that have been passed in call sequence */
-  arkspils_mem->s_pretype = pretype;
+  /* Set Spfgmr parameters that have been passed in call sequence */
+  arkspils_mem->s_pretype    = pretype;
   mxl = arkspils_mem->s_maxl = (maxl <= 0) ? ARKSPILS_MAXL : maxl;
 
-  /* Set defaults for Jacobian-related fileds */
+  /* Set defaults for Jacobian-related fields */
   arkspils_mem->s_jtimesDQ = TRUE;
   arkspils_mem->s_jtimes   = NULL;
   arkspils_mem->s_j_data   = NULL;
@@ -108,7 +107,8 @@ int ARKPcg(void *arkode_mem, int pretype, int maxl)
   arkspils_mem->s_pfree  = NULL;
   arkspils_mem->s_P_data = ark_mem->ark_user_data;
 
-  /* Set default values for the rest of the Pcg parameters */
+  /* Set default values for the rest of the Spfgmr parameters */
+  arkspils_mem->s_gstype    = MODIFIED_GS;
   arkspils_mem->s_eplifac   = ARKSPILS_EPLIN;
   arkspils_mem->s_last_flag = ARKSPILS_SUCCESS;
   ark_mem->ark_setupNonNull = FALSE;
@@ -116,8 +116,8 @@ int ARKPcg(void *arkode_mem, int pretype, int maxl)
   /* Check for legal pretype */ 
   if ((pretype != PREC_NONE) && (pretype != PREC_LEFT) &&
       (pretype != PREC_RIGHT) && (pretype != PREC_BOTH)) {
-    ARKProcessError(ark_mem, ARKSPILS_ILL_INPUT, "ARKPCG", 
-		    "ARKPcg", MSGS_BAD_PRETYPE);
+    ARKProcessError(ark_mem, ARKSPILS_ILL_INPUT, "ARKSPFGMR", 
+		    "ARKSpfgmr", MSGS_BAD_PRETYPE);
     free(arkspils_mem); arkspils_mem = NULL;
     return(ARKSPILS_ILL_INPUT);
   }
@@ -125,16 +125,16 @@ int ARKPcg(void *arkode_mem, int pretype, int maxl)
   /* Allocate memory for ytemp and x */
   arkspils_mem->s_ytemp = N_VClone(ark_mem->ark_tempv);
   if (arkspils_mem->s_ytemp == NULL) {
-    ARKProcessError(ark_mem, ARKSPILS_MEM_FAIL, "ARKPCG", 
-		    "ARKPcg", MSGS_MEM_FAIL);
+    ARKProcessError(ark_mem, ARKSPILS_MEM_FAIL, "ARKSPFGMR", 
+		    "ARKSpfgmr", MSGS_MEM_FAIL);
     free(arkspils_mem); arkspils_mem = NULL;
     return(ARKSPILS_MEM_FAIL);
   }
 
   arkspils_mem->s_x = N_VClone(ark_mem->ark_tempv);
   if (arkspils_mem->s_x == NULL) {
-    ARKProcessError(ark_mem, ARKSPILS_MEM_FAIL, "ARKPCG", 
-		    "ARKPcg", MSGS_MEM_FAIL);
+    ARKProcessError(ark_mem, ARKSPILS_MEM_FAIL, "ARKSPFGMR", 
+		    "ARKSpfgmr", MSGS_MEM_FAIL);
     N_VDestroy(arkspils_mem->s_ytemp);
     free(arkspils_mem); arkspils_mem = NULL;
     return(ARKSPILS_MEM_FAIL);
@@ -142,23 +142,23 @@ int ARKPcg(void *arkode_mem, int pretype, int maxl)
 
   /* Compute sqrtN from a dot product */
   N_VConst(ONE, arkspils_mem->s_ytemp);
-  arkspils_mem->s_sqrtN = RSqrt(N_VDotProd(arkspils_mem->s_ytemp, 
-					   arkspils_mem->s_ytemp));
+  arkspils_mem->s_sqrtN = RSqrt( N_VDotProd(arkspils_mem->s_ytemp, 
+					    arkspils_mem->s_ytemp) );
 
-  /* Call PcgMalloc to allocate workspace for Pcg */
-  pcg_mem = NULL;
-  pcg_mem = PcgMalloc(mxl, ark_mem->ark_tempv);
-  if (pcg_mem == NULL) {
-    ARKProcessError(ark_mem, ARKSPILS_MEM_FAIL, "ARKPCG", 
-		    "ARKPcg", MSGS_MEM_FAIL);
+  /* Call SpfgmrMalloc to allocate workspace for Spfgmr */
+  spfgmr_mem = NULL;
+  spfgmr_mem = SpfgmrMalloc(mxl, ark_mem->ark_tempv);
+  if (spfgmr_mem == NULL) {
+    ARKProcessError(ark_mem, ARKSPILS_MEM_FAIL, "ARKSPFGMR", 
+		    "ARKSpfgmr", MSGS_MEM_FAIL);
     N_VDestroy(arkspils_mem->s_ytemp);
     N_VDestroy(arkspils_mem->s_x);
     free(arkspils_mem); arkspils_mem = NULL;
     return(ARKSPILS_MEM_FAIL);
   }
   
-  /* Attach PCG memory to spils memory structure */
-  arkspils_mem->s_spils_mem = (void *) pcg_mem;
+  /* Attach SPFGMR memory to spils memory structure */
+  arkspils_mem->s_spils_mem = (void *) spfgmr_mem;
 
   /* Attach linear solver memory to integrator memory */
   ark_mem->ark_lmem = arkspils_mem;
@@ -168,18 +168,15 @@ int ARKPcg(void *arkode_mem, int pretype, int maxl)
 
 
 /*---------------------------------------------------------------
- ARKPcgInit:
+ ARKSpfgmrInit:
 
  This routine does remaining initializations specific to the 
- Pcg linear solver.
+ Spfgmr linear solver.
 ---------------------------------------------------------------*/
-static int ARKPcgInit(ARKodeMem ark_mem)
+static int ARKSpfgmrInit(ARKodeMem ark_mem)
 {
   ARKSpilsMem arkspils_mem;
-  PcgMem pcg_mem;
-
   arkspils_mem = (ARKSpilsMem) ark_mem->ark_lmem;
-  pcg_mem = (PcgMem) arkspils_mem->s_spils_mem;
 
   /* Initialize counters */
   arkspils_mem->s_npe = arkspils_mem->s_nli = 0;
@@ -188,17 +185,16 @@ static int ARKPcgInit(ARKodeMem ark_mem)
   arkspils_mem->s_nfes = 0;
 
   /* Check for legal combination pretype - psolve */
-  if ((arkspils_mem->s_pretype != PREC_NONE) && 
-      (arkspils_mem->s_psolve == NULL)) {
-    ARKProcessError(ark_mem, -1, "ARKPCG", 
-		    "ARKPcgInit", MSGS_PSOLVE_REQ);
+  if ((arkspils_mem->s_pretype != PREC_NONE) 
+      && (arkspils_mem->s_psolve == NULL)) {
+    ARKProcessError(ark_mem, -1, "ARKSPFGMR", "ARKSpfgmrInit", 
+		    MSGS_PSOLVE_REQ);
     arkspils_mem->s_last_flag = ARKSPILS_ILL_INPUT;
     return(-1);
   }
 
-  /* Set setupNonNull = TRUE iff there is preconditioning
-     (pretype != PREC_NONE)  and there is a preconditioning
-     setup phase (pset != NULL) */
+  /* Set setupNonNull=TRUE iff there is preconditioning (pretype != PREC_NONE)
+     and there is a preconditioning setup phase (pset != NULL)             */
   ark_mem->ark_setupNonNull = (arkspils_mem->s_pretype != PREC_NONE) 
     && (arkspils_mem->s_pset != NULL);
 
@@ -210,29 +206,26 @@ static int ARKPcgInit(ARKodeMem ark_mem)
     arkspils_mem->s_j_data = ark_mem->ark_user_data;
   }
 
-  /*  Set maxl in the PCG memory in case it was changed by the user */
-  pcg_mem->l_max = arkspils_mem->s_maxl;
-
   arkspils_mem->s_last_flag = ARKSPILS_SUCCESS;
   return(0);
 }
 
 
 /*---------------------------------------------------------------
- ARKPcgSetup:
+ ARKSpfgmrSetup:
 
- This routine does the setup operations for the Pcg linear 
+ This routine does the setup operations for the Spfgmr linear 
  solver. It makes a decision as to whether or not to signal for 
- reevaluation of Jacobian data in the pset routine, based on 
- various state variables, then it calls pset. If we signal for 
- reevaluation, then we reset jcur = *jcurPtr to TRUE, regardless 
+ re-evaluation of Jacobian data in the pset routine, based on 
+ various state variables, then it calls pset.  If we signal for 
+ re-evaluation, then we reset jcur = *jcurPtr to TRUE, regardless 
  of the pset output. In any case, if jcur == TRUE, we increment 
  npe and save nst in nstlpre.
 ---------------------------------------------------------------*/
-static int ARKPcgSetup(ARKodeMem ark_mem, int convfail, 
-		       N_Vector ypred, N_Vector fpred, 
-		       booleantype *jcurPtr, N_Vector vtemp1,
-		       N_Vector vtemp2, N_Vector vtemp3)
+static int ARKSpfgmrSetup(ARKodeMem ark_mem, int convfail, 
+			 N_Vector ypred, N_Vector fpred, 
+			 booleantype *jcurPtr, N_Vector vtemp1,
+			 N_Vector vtemp2, N_Vector vtemp3)
 {
   booleantype jbad, jok;
   realtype dgamma;
@@ -256,13 +249,12 @@ static int ARKPcgSetup(ARKodeMem ark_mem, int convfail,
 				arkspils_mem->s_P_data, vtemp1, 
 				vtemp2, vtemp3);
   if (retval < 0) {
-    ARKProcessError(ark_mem, PCG_PSET_FAIL_UNREC, "ARKPCG", 
-		    "ARKPcgSetup", MSGS_PSET_FAILED);
-    arkspils_mem->s_last_flag = PCG_PSET_FAIL_UNREC;
+    ARKProcessError(ark_mem, SPFGMR_PSET_FAIL_UNREC, "ARKSPFGMR", 
+		    "ARKSpfgmrSetup", MSGS_PSET_FAILED);
+    arkspils_mem->s_last_flag = SPFGMR_PSET_FAIL_UNREC;
   }
-  if (retval > 0) {
-    arkspils_mem->s_last_flag = PCG_PSET_FAIL_REC;
-  }
+  if (retval > 0) 
+    arkspils_mem->s_last_flag = SPFGMR_PSET_FAIL_REC;
 
   if (jbad) *jcurPtr = TRUE;
 
@@ -272,7 +264,7 @@ static int ARKPcgSetup(ARKodeMem ark_mem, int convfail,
     arkspils_mem->s_nstlpre = ark_mem->ark_nst;
   }
 
-  arkspils_mem->s_last_flag = PCG_SUCCESS;
+  arkspils_mem->s_last_flag = SPFGMR_SUCCESS;
 
   /* Return the same value that pset returned */
   return(retval);
@@ -280,39 +272,43 @@ static int ARKPcgSetup(ARKodeMem ark_mem, int convfail,
 
 
 /*---------------------------------------------------------------
- ARKPcgSolve:
+ ARKSpfgmrSolve:
 
- This routine handles the call to the generic solver PcgSolve
- for the solution of the linear system Ax = b with the PCG 
- method. The solution x is returned in the vector b.
+ This routine handles the call to the generic solver SpfgmrSolve
+ for the solution of the linear system Ax = b with the SPFGMR 
+ method, without restarts.  The solution x is returned in the 
+ vector b.
 
  If the WRMS norm of b is small, we return x = b (if this is the 
  first Newton iteration) or x = 0 (if a later Newton iteration).
 
  Otherwise, we set the tolerance parameter and initial guess 
- (x = 0), call PcgSolve, and copy the solution x into b. The 
- x-scaling and b-scaling arrays are both equal to weight.
+ (x = 0), call SpfgmrSolve, and copy the solution x into b.  The 
+ x-scaling and b-scaling arrays are both equal to weight, and no 
+ restarts are allowed.
 
- The counters nli, nps, and ncfl are incremented, and the return 
- value is set according to the success of PcgSolve. The 
- success flag is returned if PcgSolve converged, or if this is 
+ The counters nli, nps, and ncfl are incremented, and the return
+ value is set according to the success of SpfgmrSolve.  The 
+ success flag is returned if SpfgmrSolve converged, or if this is 
  the first Newton iteration and the residual norm was reduced 
  below its initial value.
 ---------------------------------------------------------------*/
-static int ARKPcgSolve(ARKodeMem ark_mem, N_Vector b, 
-		       N_Vector weight, N_Vector ynow, 
-		       N_Vector fnow)
+static int ARKSpfgmrSolve(ARKodeMem ark_mem, N_Vector b, 
+			  N_Vector weight, N_Vector ynow, 
+			  N_Vector fnow)
 {
   realtype bnorm, res_norm;
   ARKSpilsMem arkspils_mem;
-  PcgMem pcg_mem;
+  SpfgmrMem spfgmr_mem;
   int nli_inc, nps_inc, retval;
   
   arkspils_mem = (ARKSpilsMem) ark_mem->ark_lmem;
-  pcg_mem = (PcgMem) arkspils_mem->s_spils_mem;
+
+  spfgmr_mem = (SpfgmrMem) arkspils_mem->s_spils_mem;
 
   /* Test norm(b); if small, return x = 0 or x = b */
   arkspils_mem->s_deltar = arkspils_mem->s_eplifac * ark_mem->ark_eLTE; 
+
   bnorm = N_VWrmsNorm(b, weight);
   if (bnorm <= arkspils_mem->s_deltar) {
     if (ark_mem->ark_mnewt > 0) N_VConst(ZERO, b); 
@@ -323,81 +319,91 @@ static int ARKPcgSolve(ARKodeMem ark_mem, N_Vector b,
   arkspils_mem->s_ycur = ynow;
   arkspils_mem->s_fcur = fnow;
 
-  /* Set inputs delta and initial guess x = 0 to PcgSolve */
+  /* Set inputs delta and initial guess x = 0 to SpfgmrSolve */  
   arkspils_mem->s_delta = arkspils_mem->s_deltar * arkspils_mem->s_sqrtN;
   N_VConst(ZERO, arkspils_mem->s_x);
-
-  /* Call PcgSolve and copy x to b */
-  retval = PcgSolve(pcg_mem, ark_mem, arkspils_mem->s_x, b, 
-		    arkspils_mem->s_pretype, arkspils_mem->s_delta,
-		    ark_mem, weight, ARKSpilsAtimes, 
-		    ARKSpilsPSolve, &res_norm, &nli_inc, &nps_inc);
+  
+  /* Call SpfgmrSolve and copy x to b */
+  retval = SpfgmrSolve(spfgmr_mem, ark_mem, arkspils_mem->s_x, b, 
+		       arkspils_mem->s_pretype, arkspils_mem->s_gstype, 
+		       arkspils_mem->s_delta, 0, arkspils_mem->s_maxl, 
+		       ark_mem, weight, ARKSpilsAtimes, ARKSpilsPSolve, 
+		       &res_norm, &nli_inc, &nps_inc);
   N_VScale(ONE, arkspils_mem->s_x, b);
   
   /* Increment counters nli, nps, and ncfl */
   arkspils_mem->s_nli += nli_inc;
   arkspils_mem->s_nps += nps_inc;
-  if (retval != PCG_SUCCESS) arkspils_mem->s_ncfl++;
+  if (retval != SPFGMR_SUCCESS) arkspils_mem->s_ncfl++;
 
-  /* Interpret return value from SpbcgSolve */
+  /* Interpret return value from SpfgmrSolve */
   arkspils_mem->s_last_flag = retval;
 
   switch(retval) {
 
-  case PCG_SUCCESS:
-    return 0;
+  case SPFGMR_SUCCESS:
+    return(0);
     break;
-  case PCG_RES_REDUCED:
+  case SPFGMR_RES_REDUCED:
     /* allow reduction but not solution on first Newton iteration, 
        otherwise return with a recoverable failure */
     if (ark_mem->ark_mnewt == 0) return(0);
     else                         return(1);
     break;
-  case PCG_CONV_FAIL:
-    return 1;
+  case SPFGMR_CONV_FAIL:
+    return(1);
     break;
-  case PCG_PSOLVE_FAIL_REC:
-    return 1;
+  case SPFGMR_QRFACT_FAIL:
+    return(1);
     break;
-  case PCG_ATIMES_FAIL_REC:
-    return 1;
+  case SPFGMR_PSOLVE_FAIL_REC:
+    return(1);
     break;
-  case PCG_MEM_NULL:
-    return -1;
+  case SPFGMR_ATIMES_FAIL_REC:
+    return(1);
     break;
-  case PCG_ATIMES_FAIL_UNREC:
-    ARKProcessError(ark_mem, PCG_ATIMES_FAIL_UNREC, "ARKPCG", 
-		    "ARKPcgSolve", MSGS_JTIMES_FAILED);    
-    return -1;
+  case SPFGMR_MEM_NULL:
+    return(-1);
     break;
-  case PCG_PSOLVE_FAIL_UNREC:
-    ARKProcessError(ark_mem, PCG_PSOLVE_FAIL_UNREC, "ARKPCG", 
-		    "ARKPcgSolve", MSGS_PSOLVE_FAILED);
-    return -1;
+  case SPFGMR_ATIMES_FAIL_UNREC:
+    ARKProcessError(ark_mem, SPFGMR_ATIMES_FAIL_UNREC, "ARKSPFGMR", 
+		    "ARKSpfgmrSolve", MSGS_JTIMES_FAILED);    
+    return(-1);
+    break;
+  case SPFGMR_PSOLVE_FAIL_UNREC:
+    ARKProcessError(ark_mem, SPFGMR_PSOLVE_FAIL_UNREC, "ARKSPFGMR", 
+		    "ARKSpfgmrSolve", MSGS_PSOLVE_FAILED);
+    return(-1);
+    break;
+  case SPFGMR_GS_FAIL:
+    return(-1);
+    break;
+  case SPFGMR_QRSOL_FAIL:
+    return(-1);
     break;
   }
 
-  return 0;
+  return(0);
 }
 
 
 /*---------------------------------------------------------------
- ARKPcgFree:
+ ARKSpfgmrFree:
 
- This routine frees memory specific to the Pcg linear solver.
+ This routine frees memory specific to the Spfgmr linear solver.
 ---------------------------------------------------------------*/
-static void ARKPcgFree(ARKodeMem ark_mem)
+static void ARKSpfgmrFree(ARKodeMem ark_mem)
 {
   ARKSpilsMem arkspils_mem;
-  PcgMem pcg_mem;
+  SpfgmrMem spfgmr_mem;
 
   arkspils_mem = (ARKSpilsMem) ark_mem->ark_lmem;
-
+  
   N_VDestroy(arkspils_mem->s_ytemp);
   N_VDestroy(arkspils_mem->s_x);
 
-  pcg_mem = (PcgMem) arkspils_mem->s_spils_mem;
-  PcgFree(pcg_mem);
+  spfgmr_mem = (SpfgmrMem) arkspils_mem->s_spils_mem;
+  SpfgmrFree(spfgmr_mem);
 
   if (arkspils_mem->s_pfree != NULL) (arkspils_mem->s_pfree)(ark_mem);
 
@@ -407,5 +413,5 @@ static void ARKPcgFree(ARKodeMem ark_mem)
 
 
 /*---------------------------------------------------------------
-     EOF
+    EOF
 ---------------------------------------------------------------*/
