9baff4b9410fa35546ddc1cd836c8fd4357e25e5
Daniel R. Reynolds <reynolds@smu.edu>
2013-09-20 17:43:22 -0500

    Updated ARKode source files to adhere to old-style C code for legacy compilers.

--- sundials/src/arkode/arkode_butcher.c
+++ sundials/src/arkode/arkode_butcher.c
@@ -67,6 +67,12 @@ int ARKodeLoadButcherTable(int imethod, int *s, int *q, int *p,
 {
 
   int i, j;
+  realtype one = RCONST(1.0);
+  realtype two = RCONST(2.0);
+  realtype three = RCONST(3.0);
+  realtype four = RCONST(4.0);
+  realtype six = RCONST(6.0);
+  realtype sqrt2 = RSqrt(two);
 
   /* initialize output tables to zero */
   for (i=0; i<ARK_S_MAX; i++) {
@@ -611,12 +617,6 @@ int ARKodeLoadButcherTable(int imethod, int *s, int *q, int *p,
     *q = 3;
     *p = 2;
 
-    realtype one = RCONST(1.0);
-    realtype two = RCONST(2.0);
-    realtype three = RCONST(3.0);
-    realtype four = RCONST(4.0);
-    realtype six = RCONST(6.0);
-    realtype sqrt2 = RSqrt(two);
     ARK_A(A,1,0) = (two-sqrt2)/two;
     ARK_A(A,1,1) = (two-sqrt2)/two;
     ARK_A(A,2,0) = sqrt2/four;
